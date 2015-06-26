6d2e751116ccb64ded683691ea008be905086322
Sergey Romanov <xxsmotur@gmail.com>
2015-06-26 22:06:44 +0500

    add showing info as optional

--- pipupdate.py
+++ pipupdate.py
@@ -2,18 +2,21 @@ import sh
 from termcolor import colored
 import argparse
 
-def pipupdate(ver, path):
+def pipupdate(ver, path, show_info):
     command = sh.Command("{0}pip{1}".format(path, ver))
     for i, package in enumerate(command("list", "-o",_iter=True)):
         pack = package.split('(')[0]
         if len(pack) <= 20:
             print(colored('{0}. Update {1}'.format(i+1, pack), 'green'))
-            print(sh.Command("{0}pip{1}".format(path, ver))("install", "--upgrade",  pack))
+            if show_info:
+                print(sh.Command("{0}pip{1}".format(path, ver))("install", "--upgrade",  pack))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--path', nargs='?', help='Path to pip', default='/usr/bin/')
     parser.add_argument('--ver', nargs='?', help='Version of pip (2,3,3.3...)', default='3.5')
+    parser.add_argument('--show-info', help='Show installation information', action='store_true')
     args = parser.parse_args()
-    pipupdate(args.ver, args.path)
+    print(args.show_info)
+    pipupdate(args.ver, args.path, args.show_info)
